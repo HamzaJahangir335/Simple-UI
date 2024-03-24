436ea5e5ac8dcc8db045764404f521a7d15cacef
Hamza <hamzajahangir335@gmail.com>
2024-03-25 02:41:51 +0500

    some buttons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import AddUsers from './components/Users/AddUsers';
 import UsersList from './components/Users/UsersList';
 import { Fragment } from 'react';
 
-
 function App() {
   const [usersList, setUsersList] = useState([]);
   const addUserHandler = (uName, uAge) => {
@@ -11,10 +10,24 @@ function App() {
       return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toLocaleString() }];
     })
   }
+
+  const removeUser = (id) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList].filter((user) => user.id !== id)
+    });
+  }
+
+  const updateUserHandler = (updatedUser) => {
+    setUsersList((prevUsersList) => {
+      return prevUsersList.map((user) =>
+        user.id === updatedUser.id ? { name: updatedUser.name, age: updatedUser.age } : user
+      );
+    });
+  };
   return (
     <Fragment>
       <AddUsers onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
+      <UsersList removeUser = {removeUser} updateUser={updateUserHandler} users={usersList} />
     </Fragment>
   );
 }
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,19 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
-import styles from'./UsersList.module.css';
+import styles from './UsersList.module.css';
 
 const UsersList = (props) => {
+  const [editingUserId, setEditingUserId] = useState(null);
+  const [editedName, setEditedName] = useState('');
+  const [editedAge, setEditedAge] = useState('');
+
+  const editHandler = (userId) => {
+    setEditingUserId(userId);
+    const userToEdit = props.users.find((user) => user.id === userId);
+    setEditedName(userToEdit.name);
+    setEditedAge(userToEdit.age);
+  };
+
+  const cancelEditHandler = () => {
+    setEditingUserId(null);
+    setEditedName('');
+    setEditedAge('');
+  };
+
+  const saveEditHandler = () => {
+    // Call the update user function passed from the parent component
+    props.updateUser({
+      id: editingUserId,
+      name: editedName,
+      age: editedAge,
+    });
+    setEditingUserId(null);
+    setEditedName('');
+    setEditedAge('');
+  };
+
   return (
     <Card className={styles.users}>
       <ul>
-        {props.users.map((user)=>(
-            <li key={user.id}>
-                {user.name} ({user.age} years old)
-            </li>
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {editingUserId === user.id ? (
+              <div>
+                <input
+                  type="text"
+                  value={editedName}
+                  onChange={(e) => setEditedName(e.target.value)}
+                />
+                <input
+                  type="number"
+                  value={editedAge}
+                  onChange={(e) => setEditedAge(e.target.value)}
+                />
+                <button onClick={saveEditHandler}>Save</button>
+                <button onClick={cancelEditHandler}>Cancel</button>
+              </div>
+            ) : (
+              <div>
+                {user.name} ({user.age} years old){' '}
+                <div style={{ display: 'flex' }}>
+                  <button
+                    style={{ marginRight: '10px', marginTop: '10px' }}
+                    onClick={() => editHandler(user.id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    style={{ marginTop: '10px' }}
+                    onClick={() => props.removeUser(user.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            )}
+          </li>
         ))}
       </ul>
     </Card>
-  )
-}
+  );
+};
 
-export default UsersList
+export default UsersList;
